bd137d86d2edc1cad4bc21ff78ec2459ee993164
wandrade <wandrade@CGT2014120RN119.cgt.sabesp.com.br>
2013-05-13 09:48:55 -0300

    Commit inicial

---
+++ src/br/com/caiçaraspodres/teste/HelloWord.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.caiçaraspodres.teste;
+
+/**
+ *
+ * @author wandrade
+ */
+public class HelloWord {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Primeira aplicação no GIT");
+        // TODO code application logic here
+    }
+}
